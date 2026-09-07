c6ccdca173eddeb1a58d6873e2f53881ea123f57
fraa2a <123754424+fraa2a@users.noreply.github.com>
2026-09-07 04:13:38 +0200

    Fixed Background url

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,2 @@
 import './style.css'
 import 'iconify-icon'
-
-const themeButton = document.getElementById("toggle-theme");
-const savedTheme = localStorage.getItem("theme");
-const root: HTMLElement = document.documentElement;
-
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-if (savedTheme === "dark" || (!savedTheme && prefersDark)) {
-  root.dataset.theme = "dark";
-}
-
-
-themeButton!.onclick = function() {
-    
-    if (root.dataset.theme == "dark") {
-        root.dataset.theme = "light";
-        localStorage.setItem("theme", "light");
-    } else {
-        root.dataset.theme = "dark";
-        localStorage.setItem("theme", "dark");
-    }
-}
